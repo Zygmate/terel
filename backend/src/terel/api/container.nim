3c82710c22c697d714243f04bd5f48837c2438bd
Akito <the@akito.ooo>
2024-01-15 16:41:49 +0100

    Add Payload Containers

--- backend/src/terel/api/container.nim
+++ backend/src/terel/api/container.nim
@@ -29,3 +29,30 @@ func containRace*(races: openArray[JsonNode]): PayloadContainer =
     `type`: Race,
     data: @races
   )
+
+func containBackground*(backgrounds: openArray[JsonNode]): PayloadContainer =
+  PayloadContainer(
+    id: -1,
+    status: OK,
+    msg: "",
+    `type`: Background,
+    data: @backgrounds
+  )
+
+func containSpell*(spells: openArray[JsonNode]): PayloadContainer =
+  PayloadContainer(
+    id: -1,
+    status: OK,
+    msg: "",
+    `type`: Spell,
+    data: @spells
+  )
+
+func containTech*(techs: openArray[JsonNode]): PayloadContainer =
+  PayloadContainer(
+    id: -1,
+    status: OK,
+    msg: "",
+    `type`: Tech,
+    data: @techs
+  )
