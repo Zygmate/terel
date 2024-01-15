9b5654a459293cadae692b18f66d2ca8b961124a
Akito <the@akito.ooo>
2024-01-15 16:39:43 +0100

    Add Payload Container Utilities

--- backend/src/terel/api/container.nim
+++ backend/src/terel/api/container.nim
@@ -3,8 +3,29 @@ from std/json import JsonNode
 type
   ResponseStatus* = enum
     OK, ERROR
+  ResponseType* = enum
+    Race, Background, Spell, Tech
   PayloadContainer* = object
-    id: int
-    status: ResponseStatus
-    msg: string
-    data: seq[JsonNode]
+    id*: int
+    status*: ResponseStatus
+    msg*: string
+    `type`*: ResponseType
+    data*: seq[JsonNode]
+
+func containError*(races: openArray[JsonNode], msg = "Error occurred."): PayloadContainer =
+  PayloadContainer(
+    id: -1,
+    status: ERROR,
+    msg: msg,
+    `type`: Race,
+    data: @[]
+  )
+
+func containRace*(races: openArray[JsonNode]): PayloadContainer =
+  PayloadContainer(
+    id: -1,
+    status: OK,
+    msg: "",
+    `type`: Race,
+    data: @races
+  )
