ac42026a7d23495c5d3be800acb55f810911f798
Akito <the@akito.ooo>
2024-01-15 16:31:53 +0100

    Add Race related API Endpoints

--- backend/src/terel.nim
+++ backend/src/terel.nim
@@ -13,8 +13,12 @@ import
   terel/model/[
     #configuration,
   ],
+  terel/data/[
+    race,
+  ],
   std/[
     segfaults,
+    json
   ],
   pkg/[
     #zero_functional
@@ -37,13 +41,58 @@ when isMainModule:
   logger.log(lvlNotice, "appRevision: " & appRevision)
   logger.log(lvlNotice, "appDate:     " & appDate)
 
-  proc testConnection(request: Request) =
+  proc handlerTest(request: Request) =
     initHeaders
     request.respond(httpCodeOK, headers, """{ "msg": "Hello, World!" }""")
 
+  proc handlerRace(request: Request) =
+    initHeaders
+    request.respond(httpCodeOK, headers, """{ "msg": "Hello, World!" }""")
+
+  proc handlerRaceGnome(request: Request) =
+    initHeaders
+    request.respond(httpCodeOK, headers, $ %createGnome())
+
+  proc handlerRaceDwarf(request: Request) =
+    initHeaders
+    request.respond(httpCodeOK, headers, $ %createDwarf())
+
+  proc handlerRaceElf(request: Request) =
+    initHeaders
+    request.respond(httpCodeOK, headers, $ %createElf())
+
+  proc handlerRaceHuman(request: Request) =
+    initHeaders
+    request.respond(httpCodeOK, headers, $ %createHuman())
+
+  proc handlerRaceHalfling(request: Request) =
+    initHeaders
+    request.respond(httpCodeOK, headers, $ %createHalfling())
+
+  proc handlerRaceHalfOgre(request: Request) =
+    initHeaders
+    request.respond(httpCodeOK, headers, $ %createHalfOgre())
+
+  proc handlerRaceHalfOrc(request: Request) =
+    initHeaders
+    request.respond(httpCodeOK, headers, $ %createHalfOrc())
+
+  proc handlerRaceHalfElf(request: Request) =
+    initHeaders
+    request.respond(httpCodeOK, headers, $ %createHalfElf())
+
   var router: Router
-  router.get("/test", testConnection)
+  router.get("/test", handlerTest)
+  router.get("/race", handlerRace)
+  router.get("/race/gnome", handlerRaceGnome)
+  router.get("/race/dwarf", handlerRaceDwarf)
+  router.get("/race/elf", handlerRaceElf)
+  router.get("/race/human", handlerRaceHuman)
+  router.get("/race/halfling", handlerRaceHalfling)
+  router.get("/race/halfogre", handlerRaceHalfOgre)
+  router.get("/race/halforc", handlerRaceHalfOrc)
+  router.get("/race/halfelf", handlerRaceHalfElf)
 
   let server = newServer(router)
-  logger.log lvlInfo, "Serving on http://127.0.0.1:" & $serverPort
+  logger.log lvlNotice, "Serving on http://127.0.0.1:" & $serverPort
   server.serve(serverPort.Port)
