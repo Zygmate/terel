6fe42423aaf1d83635c6c979da9ad7692e778601
Akito <the@akito.ooo>
2024-01-15 17:44:19 +0100

    Add Background

---
+++ backend/src/terel/data/background.nim
@@ -0,0 +1,102 @@
+type
+  Background* = object of RootObj
+    id*: int
+    name*: string
+    strength*: int
+    intelligence*: int
+    constitution*: int
+    willpower*: int
+    dexterity*: int
+    perception*: int
+    beauty*: int
+    charisma*: int
+    hasGender*: bool
+    technical*: int
+    magickal*: int
+    bow*: int
+    dodge*: int
+    melee*: int
+    throwing*: int
+    backstab*: int
+    pickpocket*: int
+    prowling*: int
+    spottrap*: int
+    gambling*: int
+    haggle*: int
+    heal*: int
+    persuasion*: int
+    techMod*: int
+    repair*: int
+    firearms*: int
+    pickLock*: int
+    disarmtrap*: int
+    physDmgRes*: int
+    fatigueDmgRes*: int
+    magickRes*: int
+    fireRes*: int
+    poisonRes*: int
+    electricalRes*: int
+    critSuccessChance*: int
+  ApprenticedToABlacksmith* = object of Background
+  ApprenticedToAShopkeeper* = object of Background
+  BeatWithAnUglyStick* = object of Background
+  BornUnderASign* = object of Background
+  ChildOfAHero* = object of Background
+  DarkSight* = object of Background
+  Educator* = object of Background
+  ExtremePersonality* = object of Background
+  Hyperactive* = object of Background
+  Inheritance* = object of Background
+  MadDoctor* = object of Background
+  MiracleOperation* = object of Background
+  NietzschePosterChild* = object of Background
+  OnlyChild* = object of Background
+  ProfessionalKnifeTosser* = object of Background
+  RaisedByMonks* = object of Background
+  RaisedBySnakeHandlers* = object of Background
+  RaisedInThePits* = object of Background
+  RanAwayWithTheCircus* = object of Background
+  Sickly* = object of Background
+  SoldYourSoul* = object of Background
+  SpecialPerson* = object of Background
+  TrollOffspring* = object of Background
+  AfraidOfTheDark* = object of Background
+  AfraidOfTheDarkAgoraphobic* = object of Background
+  ArmyTraining* = object of Background
+  Arsonist* = object of Background
+  Bandit* = object of Background
+  Barbarian* = object of Background
+  Bookworm* = object of Background
+  BrideOfFrankenstein* = object of Background
+  Bully* = object of Background
+  CharlatansProtege* = object of Background
+  ClanlessDwarf* = object of Background
+  DarkElfFollower* = object of Background
+  DayMage* = object of Background
+  DisenfranchisedGnome* = object of Background
+  ElvenBlood* = object of Background
+  EscapedLunatic* = object of Background
+  FactoryEscapee* = object of Background
+  FeralChild* = object of Background
+  FoppishElf* = object of Background
+  FrankensteinMonster* = object of Background
+  FreedBodyguard* = object of Background
+  HalflingOrphan* = object of Background
+  Hydrophobic* = object of Background
+  IdiotSavant* = object of Background
+  LadysMan* = object of Background
+  MagickAllergy* = object of Background
+  NatureMage* = object of Background
+  NightMage* = object of Background
+  RaisedByElves* = object of Background
+  RaisedByOrcs* = object of Background
+  RareHalfOgreBirth* = object of Background
+  SentToCharmSchool* = object of Background
+  ShelteredChildhood* = object of Background
+  SkyMage* = object of Background
+  SuperModel* = object of Background
+  SuppressedOrcishLooks* = object of Background
+  Technophobia* = object of Background
+  Tomboy* = object of Background
+  ToughHide* = object of Background
+  WildHalfOgre* = object of Background
