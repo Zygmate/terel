a260078bd349fa69db0228a2aeed5781cd971d73
edoardovicenzi <48050856+edoardovicenzi@users.noreply.github.com>
2024-06-16 21:59:22 +0200

    NEW configuration for fetch requests

--- frontend/terel-app/src/pages/AboutUs.jsx
+++ frontend/terel-app/src/pages/AboutUs.jsx
@@ -1,23 +1,19 @@
+import { fetch } from '@tauri-apps/plugin-http';
 import { useState } from "react"
 
 export default function AboutUs (){
 const [data, setData] = useState({})
 
-function handleTest() {
-    fetch("http://127.0.0.1:58111/background/NightMage")
+    //TODO: THis function was a test
+async function handleTest() {
+    const response = await fetch("http://127.0.0.1:1337/background/NightMage")
         .then((res) => res.json())
-        .then((data) => {
-            console.log(data)
-            setData(data)
-        })
-    .catch((err) => {
-        console.error(err.message)
-    })
+        .then((raw) => setData(raw.data))
+
+        console.log(data)
 }
     return (
         <div className="outlet-wrapper">
-            <button onClick={() => handleTest()}>Test</button>
-            <pre style={"border: 1px solid red"}>{data}</pre>
         </div>
     )
 }
