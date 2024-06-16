8fb71de1f1a6961e21d0dfb0fa07ab094b5ca598
edoardovicenzi <48050856+edoardovicenzi@users.noreply.github.com>
2024-06-16 18:09:46 +0200

    Backend test

--- frontend/terel-app/src/pages/AboutUs.jsx
+++ frontend/terel-app/src/pages/AboutUs.jsx
@@ -1,7 +1,23 @@
-export default function AboutUs (){
-    return (
-    <div className="outlet-wrapper">
+import { useState } from "react"
 
+export default function AboutUs (){
+const [data, setData] = useState({})
+
+function handleTest() {
+    fetch("http://127.0.0.1:58111/background/NightMage")
+        .then((res) => res.json())
+        .then((data) => {
+            console.log(data)
+            setData(data)
+        })
+    .catch((err) => {
+        console.error(err.message)
+    })
+}
+    return (
+        <div className="outlet-wrapper">
+            <button onClick={() => handleTest()}>Test</button>
+            <pre style={"border: 1px solid red"}>{data}</pre>
         </div>
     )
 }
